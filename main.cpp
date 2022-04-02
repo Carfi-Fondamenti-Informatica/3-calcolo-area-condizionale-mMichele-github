36b434b1ea02cf5067318a88f97d98a131abe7cf
mMichele-github <100371043+mMichele-github@users.noreply.github.com>
2022-04-02 10:52:08 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main()
+{
+    float a=0, b=0;
+    int opzione=0;
+    cout << "Inserire due valori reali positivi a piacere:" << endl;
+    cin >>a>>b;
+    if (a>=0 && b>=0)
+    {
+        cout << "Scegliere un'opzione tra 0, 1, 2 :" << endl;
+        cin >>opzione;
+        switch(opzione)
+        {
+            case 0:
+                cout << (a * b) / 2 << endl;
+                break;
+            case 1:
+                cout << a * a << endl;
+                break;
+            case 2:
+                cout << a * b << endl;
+                break;
+            default:
+                cout << "Opzione non valida" << endl;
+        }
+    }
+    else
+    {
+        cout << "I valori inseriti non sono validi" << endl;
+        return 0;
+    }
+    return 0;
 }
